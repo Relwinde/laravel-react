f520745d9053f65deefa1bd4e6fd2f1e7ef55bb1
Relwinde <ouedgv@hotmail.fr>
2023-08-07 20:48:30 +0100

    first commit ever

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        THIS IS THE APP
+      </div>
+    </>
+  )
+}
+
+export default App
