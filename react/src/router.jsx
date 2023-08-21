047610ba4c33092f922bdb69cd1461721e2b540f
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2023-08-21 01:17:37 +0000

    error: users is null, going to bed

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -6,6 +6,7 @@ import NotFound from "./views/notfound";
 import DefaultLayout from "./components/DefaultLayout";
 import GuestLayout from "./components/GuestLayout";
 import Dashboard from "./views/Dashboard";
+import UserForm from "./views/UserForm";
 
 
 const router = createBrowserRouter ([
@@ -20,6 +21,14 @@ const router = createBrowserRouter ([
             {
                 path: '/users',
                 element : <Users/>,
+            },
+            {
+                path: '/users/new',
+                element : <UserForm/>,
+            },
+            {
+                path: '/users/:id',
+                element : <UserForm/>,
             },
             {
                 path: '/dashboard',
--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import axiosClient from "../axios-client"
+import { Link } from "react-router-dom"
 
 export default function Users (){
 
@@ -7,32 +8,65 @@ export default function Users (){
     //A loading text untill the users are actually loaded
     const [loading, setLoading] = useState(false)
 
-
-    // Because of strict mode in development mode this code will be executed twice
-    useEffect(() =>{
-        getUsers()
-    }, [])
-
     const getUsers = () => {
         setLoading(true)
         axiosClient.get('/users')
             .then(({data})=> {
                 setLoading(false)
+                setUsers(data.data)
                 console.log(data)
+                
             })
             .catch(()=>{
                 setLoading(false)
             })
     }
 
+    useEffect(() =>{
+        getUsers()
+    }, [])
+
+   
+
+
+    // Because of strict mode in development mode this code will be executed twice
+    
 
 
     return (
         <div>           
-            <div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: "center" }}>
                 <h1>Users</h1>
+                <Link to= '/users/new' className="btn-add">Add new</Link>
+            </div>
 
-                <Link to= >Add new</Link>
+            <div className="card animated fadeInDown">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Create Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(u => (
+                                <tr>
+                                    <td>{u.id}</td>
+                                    <td>{u.name}</td>
+                                    <td>{u.email}</td>
+                                    <td>{u.created_at}</td>
+                                    <td><Link to={'users/'+u.id}>Edit</Link></td>
+                                    <button  className="btn-delete">DELETE</button>
+                                </tr>
+                            ))
+                        }
+                       
+                    </tbody>
+                </table>
+
             </div>
         </div>
     )
